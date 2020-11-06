9772f7e888301befc2eb2a3d8b41ed57bf9d5721
surtono <surtono@florentcuchet.com>
2020-11-06 16:47:54 +0700

    2

---
+++ app/Settings/Playlist.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Settings;
+
+class Playlist
+{
+  public static function get(){
+    return [
+      'populer' => [
+        'title' => 'Populer',
+        'id' => 'PLFgquLnL59alQ4PrI-9tZyl0Z8Bqp-RE7',
+      ],
+      'terbaru' => [
+        'title' => 'Terbaru',
+        'id' => 'PLFgquLnL59anIEZe7OonXASn8v_W8UoTW',
+      ],
+      'dangdut' => [
+        'title' => 'Dangdut',
+        'id' => 'PLK-euiEs1S7tOmRTp01UINg8QZcwWk3_1',
+      ],
+      'hits' => [
+        'title' => 'Hits',
+        'id' => 'PLFgquLnL59alQ4PrI-9tZyl0Z8Bqp-RE7',
+      ],
+      'baru' => [
+        'title' => 'Baru ',
+        'id' => 'PLFgquLnL59anIEZe7OonXASn8v_W8UoTW',
+      ],
+    ];
+  }
+}
